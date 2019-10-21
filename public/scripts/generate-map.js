9d941adf896416443a216ad2b0ee2cd80b6dc008
Gabi Geollegue <gabigeollegue@gmail.com>
2019-10-21 01:12:57 +0000

    add some comments

--- public/scripts/generate-map.js
+++ public/scripts/generate-map.js
@@ -1,10 +1,6 @@
+/* eslint-disable no-undef */
 /* eslint-disable func-style */
-// This example adds a search box to a map, using the Google Place Autocomplete
-// feature. People can enter geographical searches. The search box will return a
-// pick list containing a mix of places and predicted search terms.
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+
 function initAutocomplete() {
   let map = new google.maps.Map(document.getElementById('map'), {
     center: {
@@ -43,7 +39,7 @@ function initAutocomplete() {
     console.log("all Places ", allPlaces, allPlaces.length);
 
 
-    places.forEach(function (place) {
+    places.forEach(function(place) {
       //saves each search to the array, maybe connect to an event handler so only locations that user WANTS saved is added to this array
       allPlaces.push(places[0]);
 
@@ -90,7 +86,8 @@ function initAutocomplete() {
       marker.addListener('click', function() {
         infowindow.open(map, marker);
       });
-
     });
   });
 }
+
+
